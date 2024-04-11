38221a7c67698547b1c7e2782dcceecb92fdc1ca
mikiahcarnes <mcarnes76347@ucumberlands.edu>
2024-04-11 07:07:13 -0400

    Adding route stuff

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Trail = require("./models/trail");
 
 mongoose.connect("mongodb://localhost:27017/trail-camp");
@@ -16,13 +17,48 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/trails", async (req, res) => {
   const trails = await Trail.find({});
-  res.render("trials/index");
+  res.render("trails/index", { trails });
+});
+
+app.get("/trails/new", (req, res) => {
+  res.render("trails/new");
+});
+
+app.post("/trails", async (req, res) => {
+  const trail = new Trail(req.body.trail);
+  await trail.save();
+  res.redirect(`/trails/${trail._id}`);
+});
+
+app.get("/trails/:id", async (req, res) => {
+  const trail = await Trail.findById(req.params.id);
+  res.render("trails/show", { trail });
+});
+
+app.get("/trails/:id/edit", async (req, res) => {
+  const trail = await Trail.findById(req.params.id);
+  res.render("trails/edit", { trail });
+});
+
+app.put("/trails/:id", async (req, res) => {
+  const { id } = req.params;
+  const trail = await Trail.findByIdAndUpdate(id, { ...req.body.trail });
+  res.redirect(`/trails/${trail._id}`);
+});
+
+app.delete("/trails/:id", async (req, res) => {
+  const { id } = req.params;
+  await Trail.findByIdAndDelete(id);
+  res.redirect("/trails");
 });
 
 app.listen(3000, () => {
