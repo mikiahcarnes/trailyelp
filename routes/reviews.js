d4238626036feaf2d5b7fb0f919cdb15bd759e4a
mikiahcarnes <mcarnes76347@ucumberlands.edu>
2024-04-25 02:18:12 -0400

    start script

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,15 +5,15 @@ const Trail = require("../models/trail");
 const reviews = require("../controllers/reviews");
 const Review = require("../models/review");
 const ExpressError = require("../utils/ExpressError");
-const CatchAsync = require("../utils/catchAsync");
+const catchAsync = require("../utils/catchAsync");
 
-router.post("/", isLoggedIn, validateReview, CatchAsync(reviews.createReview));
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  CatchAsync(reviews.deleteReview)
+  catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
--- routes/trails.js
+++ routes/trails.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const trails = require("../controllers/trails");
-const CatchAsync = require("../utils/catchAsync");
+const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateTrail } = require("../middleware");
 const multer = require("multer");
 const { storage } = require("../cloudinary");
@@ -11,33 +11,33 @@ const Trail = require("../models/trail");
 
 router
   .route("/")
-  .get(CatchAsync(trails.index))
+  .get(catchAsync(trails.index))
   .post(
     isLoggedIn,
     upload.array("image"),
     validateTrail,
-    CatchAsync(trails.createTrail)
+    catchAsync(trails.createTrail)
   );
 
 router.get("/new", isLoggedIn, trails.renderNewForm);
 
 router
   .route("/:id")
-  .get(CatchAsync(trails.showTrail))
+  .get(catchAsync(trails.showTrail))
   .put(
     isLoggedIn,
     isAuthor,
     upload.array("image"),
     validateTrail,
-    CatchAsync(trails.updateTrail)
+    catchAsync(trails.updateTrail)
   )
-  .delete(isLoggedIn, isAuthor, CatchAsync(trails.deleteTrail));
+  .delete(isLoggedIn, isAuthor, catchAsync(trails.deleteTrail));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
-  CatchAsync(trails.renderEditForm)
+  catchAsync(trails.renderEditForm)
 );
 
 module.exports = router;
